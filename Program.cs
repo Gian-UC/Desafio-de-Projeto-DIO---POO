67bac227e54b8bafe2118d01c05e021fc9974b5b
Giancarlo Salomone <g.salomone@live.com>
2023-11-09 20:26:25 -0300

    ToDos Modificados com sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Teste com a classe Nokia
+        Nokia nokia = new Nokia("123456789", "Nokia Modelo", "123456789012345", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Teste com a classe Iphone
+        Iphone iphone = new Iphone("987654321", "iPhone Modelo", "987654321098765", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
